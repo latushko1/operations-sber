e778f34cebee929527d57a83b9d1d9cccbd48ade
Egor <98906212+latushko1@users.noreply.github.com>
2024-07-04 17:15:26 +0600

    Update get_operations.py

--- get_operations.py
+++ get_operations.py
@@ -1,22 +1,12 @@
 import re
 import textwrap
 import json
+from datetime import datetime
 
-from jinja2 import Template
-
-render_template = Template(
-    """
-    {{ date }} {{ description }}
-    {{ from_operation_type|default('Пустое значение для "from"', true) }} {{from_operation_number}} -> Счет {{ to }}
-    {{ operation_amount }} {{ operation_currency }}
-    """
-)
-
-
-def get_operation(operation):
+def format_operation(operation):
     from_operation_type = ''
     from_operation_number = ''
-    data_operation = operation['date'].split("T")[0]
+    data_operation = datetime.strptime(operation['date'].split("T")[0], "%Y-%m-%d").strftime("%d.%m.%Y")
     type_operation = operation['description']
     if 'from' in operation:
         from_operation_parts = operation['from'].split(" ")
@@ -31,25 +21,31 @@ def get_operation(operation):
                 from_operation_number[index] = re.sub(r'.{4}$', '****', fragment)
         from_operation_number = " ".join(from_operation_number)
 
-    to_operation = operation['to'].split(" ")[1][-4:]
+    if 'Счет' in operation['to']:
+        to_operation = '**' + operation['to'].split(" ")[1][-4:]
+    else:
+        to_operation = operation['to']
+
     operation_amount = operation['operationAmount']['amount']
     operation_currency = operation['operationAmount']['currency']['name']
 
-    return render_template.render(date=data_operation, description=type_operation,
-                                  from_operation_type=from_operation_type,
-                                  from_operation_number=from_operation_number, to=to_operation,
-                                  operation_amount=operation_amount, operation_currency=operation_currency)
+    result = f"""
+{data_operation} {type_operation}
+{from_operation_type or 'Пустое значение для "from"'} {from_operation_number} -> {to_operation}
+{operation_amount} {operation_currency}
+"""
+    return result.strip()
 
 
 if __name__ == "__main__":
-    with open("operations.json", 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    data = sorted(data, key=lambda x: x.get("date", ''), reverse=True)
+    try:
+        with open(r"path_to_operation_file", 'r', encoding='utf-8') as file:
+            operations = json.load(file)
+    except FileNotFoundError:
+        print("Файл операциями не найден.")
+    else:
+        data = sorted(operations, key=lambda x: x.get("date", ''), reverse=True)
+        executed_operations = [item for item in data if item.get('state') == 'EXECUTED']
 
-    for idx, item in enumerate(data):
-        if item['state'] == 'EXECUTED':
-            print(get_operation(item))
-            if idx == 5:
-                break
-
-    # print(get_operation(data))
+        for idx, item in enumerate(executed_operations[:5]):
+            print(format_operation(item)+ "\n")
